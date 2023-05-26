f731753d7f148ce454d0776cd4a3b8ea8d2d9f72
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2023-05-25 21:55:52 -0400

    Time -> 1:00:41, EngineeringMeLaptop, 05-25-2023

--- src/main.js
+++ src/main.js
@@ -1,2 +1,58 @@
 'use strict';
 let shop = document.getElementById('shop');
+
+let shopItemsData = [
+  {
+    id: 'idName1',
+    name: 'Casual Shirt',
+    price: 45,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    img: '/images/img-1.jpg',
+  },
+  {
+    id: 'idName2',
+    name: 'Office Shirt',
+    price: 100,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero aliquid doloremque minus..',
+    img: '/images/img-2.jpg',
+  },
+  {
+    id: 'idName3',
+    name: 'T Shirt',
+    price: 25,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed!',
+    img: '/images/img-3.jpg',
+  },
+  {
+    id: 'idName4',
+    name: 'Mens Suit',
+    price: 400,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur.',
+    img: '/images/img-4.jpg',
+  },
+];
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map(x => {
+      return `
+  <div class="item">
+        <img width="220" src="${x.img}" alt="" />
+        <div class="details">
+          <h3>${x.name}</h3>
+          <p>${x.desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${x.price}</h2>
+            <div class="buttons">
+              <i class="bi bi-dash-lg"></i>
+              <div class="quantity">0</div>
+              <i class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    })
+    .join(''));
+};
+
+generateShop();
