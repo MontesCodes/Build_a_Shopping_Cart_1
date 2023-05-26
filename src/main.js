5aaf2bd4db20b5489f6f1f1bd6521a500ab0f71e
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2023-05-25 22:02:11 -0400

    Time -> 1:05:19, EngineeringMeLaptop, 05-25-2023

--- src/main.js
+++ src/main.js
@@ -35,17 +35,18 @@ let shopItemsData = [
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map(x => {
+      let { id, name, price, desc, img } = x;
       return `
-  <div class="item">
-        <img width="220" src="${x.img}" alt="" />
+  <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="" />
         <div class="details">
-          <h3>${x.name}</h3>
-          <p>${x.desc}</p>
+          <h3>${name}</h3>
+          <p>${desc}</p>
           <div class="price-quantity">
-            <h2>$ ${x.price}</h2>
+            <h2>$ ${price}</h2>
             <div class="buttons">
               <i class="bi bi-dash-lg"></i>
-              <div class="quantity">0</div>
+              <div id=${id} class="quantity">0</div>
               <i class="bi bi-plus-lg"></i>
             </div>
           </div>
