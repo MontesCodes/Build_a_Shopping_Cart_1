1df2958c97ba6417102b61829e5330d85b456c44
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2023-05-26 19:27:00 -0400

    Time -> 2:02:34, EngineeringMeLaptop, 05-26-2023

--- src/main.js
+++ src/main.js
@@ -30,6 +30,90 @@ let shopItemsData = [
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur.',
     img: '/images/img-4.jpg',
   },
+  {
+    id: 'idName5',
+    name: 'Mens Suit',
+    price: 400,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur.',
+    img: '/images/img-4.jpg',
+  },
+  {
+    id: 'idName6',
+    name: 'Casual Shirt',
+    price: 45,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    img: '/images/img-1.jpg',
+  },
+  {
+    id: 'idName7',
+    name: 'Office Shirt',
+    price: 100,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero aliquid doloremque minus..',
+    img: '/images/img-2.jpg',
+  },
+  {
+    id: 'idName8',
+    name: 'T Shirt',
+    price: 25,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed!',
+    img: '/images/img-3.jpg',
+  },
+  {
+    id: 'idName9',
+    name: 'Casual Shirt',
+    price: 45,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    img: '/images/img-1.jpg',
+  },
+  {
+    id: 'idName10',
+    name: 'Office Shirt',
+    price: 100,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero aliquid doloremque minus..',
+    img: '/images/img-2.jpg',
+  },
+  {
+    id: 'idName11',
+    name: 'T Shirt',
+    price: 25,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed!',
+    img: '/images/img-3.jpg',
+  },
+  {
+    id: 'idName12',
+    name: 'Mens Suit',
+    price: 400,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur.',
+    img: '/images/img-4.jpg',
+  },
+  {
+    id: 'idName13',
+    name: 'Mens Suit',
+    price: 400,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur.',
+    img: '/images/img-4.jpg',
+  },
+  {
+    id: 'idName14',
+    name: 'Casual Shirt',
+    price: 45,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    img: '/images/img-1.jpg',
+  },
+  {
+    id: 'idName15',
+    name: 'Office Shirt',
+    price: 100,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero aliquid doloremque minus..',
+    img: '/images/img-2.jpg',
+  },
+  {
+    id: 'idName16',
+    name: 'T Shirt',
+    price: 25,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed!',
+    img: '/images/img-3.jpg',
+  },
 ];
 
 let basket = JSON.parse(localStorage.getItem('dataStorage')) || [];
@@ -76,9 +160,8 @@ let increment = id => {
     search.item += 1;
   }
 
-  localStorage.setItem('dataStorage', JSON.stringify(basket));
-
   update(selectedItem.id);
+  localStorage.setItem('dataStorage', JSON.stringify(basket));
 };
 let decrement = id => {
   let selectedItem = id;
@@ -89,9 +172,9 @@ let decrement = id => {
     search.item -= 1;
   }
 
-  localStorage.setItem('dataStorage', JSON.stringify(basket));
-
   update(selectedItem.id);
+  basket = basket.filter(m => m.item !== 0);
+  localStorage.setItem('dataStorage', JSON.stringify(basket));
 };
 let update = id => {
   let search = basket.find(x => x.id === id);
