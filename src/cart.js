71a37e4f22798fe3a571eb8ac408163f69ac49b8
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2023-05-26 20:23:27 -0400

    Time -> 2:14:43, EngineeringMeLaptop, 05-26-2023

--- src/cart.js
+++ src/cart.js
@@ -1,1 +1,27 @@
 'use strict';
+let label = document.getElementById('label');
+let shoppingCart = document.getElementById('shopping-cart');
+
+let basket = JSON.parse(localStorage.getItem('dataStorage')) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById('cartAmount');
+  cartIcon.innerHTML = basket.map(m => m.item).reduce((e, s) => e + s, 0);
+};
+
+calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="index.html">
+      <button class="HomeBtn">Back to home</button>
+    </a>
+    `;
+  }
+};
+
+generateCartItems();
